bd84cb42a9549ee8a56efc9f2248d21932ef5702
sejin <neokin7734@naver.com>
2024-09-27 12:14:51 +0900

    init project

---
+++ src/main/java/com/example/reaction_time/ReactionTimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.reaction_time;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactionTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactionTimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reaction_time/ReactionTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reaction_time;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactionTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
