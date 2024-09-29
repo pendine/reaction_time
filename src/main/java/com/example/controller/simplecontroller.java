e5af554c5f6cb3f0093ba9dc4d6b91ccb912d25d
sejin <neokin7734@naver.com>
2024-09-29 20:28:51 +0900

    컨트롤러 작성

--- src/main/java/com/example/controller/simplecontroller.java
+++ src/main/java/com/example/controller/simplecontroller.java
@@ -1,13 +1,16 @@
 package com.example.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class simplecontroller {
 
     @GetMapping("/")
-    public String welcome() {
-        return "welcome";
+    public String startTest() {
+        return "startTest";
     }
+
+
+
 }
